9f5ed17368b6d0cdea421bc560d5fbdc0fb24b7f
achu <ashleywchu@gmail.com>
2016-09-12 17:13:58 -0400

    [snake] make changes so that game can start with spacebar; fix game loop; change how snake's direction and moves were being made

--- snake.js
+++ snake.js
@@ -14,13 +14,30 @@ function main() {
 
   addEventListener( "keydown", function(event) {
     event.preventDefault();
-    if ( event.keyCode in KEYS ) {
-      snake.direction = KEYS[event.keyCode];
+    if ( event.keyCode == 32 ) {
+      game.start();
     }
+    snake.setDirection( event.keyCode );
   }, false);
 
-  init();
   loop();
+}
+
+function loop() {
+  setTimeout( function() {
+    requestAnimationFrame(loop);
+    game.clearBoard();
+    if (game.over == false) {
+      if ( snake.collision == true ) {
+        game.stop();
+      } else {
+        snake.move( snake.direction );
+        draw();
+      }
+    } else {
+      init();
+    }
+  }, 1000 / game.fps);
 }
 
 function init() {
@@ -31,16 +48,9 @@ function init() {
   food.init();
 }
 
-function loop() {
-    game.clearBoard();
-    snake.move( snake.direction );
-    draw();
-  requestAnimationFrame(loop);
-}
-
 function draw() {
-  var dx = canvas.width / columns,
-      dy = canvas.height / rows;
+  var dx = canvas.width / COLUMNS,
+      dy = canvas.height / ROWS;
 
   snake.draw(dx,dy);
   food.draw(dx,dy);
@@ -51,8 +61,10 @@ function Game() {}
 
 Game.prototype.init = function() {
   this.score = 0;
-  this.over = false;
+  this.over = true;
   game.clearBoard();
+  this.fps = 20;
+  game.showStartMessage();
 }
 
 Game.prototype.start = function() {
@@ -68,8 +80,14 @@ Game.prototype.clearBoard = function() {
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+Game.prototype.showStartMessage = function() {
+  context.font = "40px sans-serif";
+  context.fillStyle= "grey";
+  context.fillText("Space bar to start", canvas.width / 4, canvas.height / 2);
+}
+
 /////////////// SNAKE ///////////////
-// Each snake segment is stored in queue. Head is at the front of queue
+// Each snake segment is stored in queue. Head is the first set of coordinates that can be found at the front of queue, an array that represents the snake's body.
 function Snake() {
   this.head = null,
   this.queue = [],
@@ -78,11 +96,12 @@ function Snake() {
 
 Snake.prototype.init = function(params) {
   this.head = { x: params.x, y: params.y };
-  this.direction = KEYS.RIGHT;
+  this.direction = "right";
   this.queue = [];
   this.length = this.queue.length;
-  snake.add( params.x, params.y )
-  this.newHead = this.head;
+  this.nextHead = this.head;
+  this.collision = false;
+  snake.add( params.x, params.y );
 }
 
 Snake.prototype.draw = function(dx,dy) {
@@ -93,53 +112,50 @@ Snake.prototype.draw = function(dx,dy) {
   }
 }
 
+// Sets the snake's direction
+Snake.prototype.setDirection = function(keyCode) {
+  if( keyCode == 40 && this.direction !== "up" ) this.direction = "down";
+  else if( keyCode == 38 && this.direction !== "down" ) this.direction = "up";
+  else if( keyCode == 39 && this.direction !== "left" ) this.direction = "right";
+  else if( keyCode == 37 && this.direction !== "right" ) this.direction = "left";
+}
+
 // Moves the snake based on snake's direction
 Snake.prototype.move = function(direction) {
   switch (direction) {
+    // up
     case "up":
-      if (this.direction !== KEYS.DOWN ) {
-        snake.updatePosition( 0, -10 );
-      };
+      snake.updatePosition( 0, -10 );
       break;
     case "down":
-      if (this.direction !== KEYS.UP ) {
-        snake.updatePosition( 0, 10 );
-      };
+      snake.updatePosition( 0, 10 );
       break;
     case "left":
-      if (this.direction !== KEYS.RIGHT ) {
-        snake.updatePosition( -10, 0 );
-      };
+      snake.updatePosition( -10, 0 );
       break;
     case "right":
-      if (this.direction !== KEYS.LEFT ) {
-        snake.updatePosition( 10, 0 );
-      };
+      snake.updatePosition( 10, 0 );
       break;
   }
   snake.checkCollision();
   snake.grow();
-  snake.add(snake.newHead.x, snake.newHead.y);
+  snake.add(snake.nextHead.x, snake.nextHead.y);
 }
 
 // Update the coordinates of snake's head
 Snake.prototype.updatePosition = function(x,y) {
-  this.newHead.x = (this.newHead.x += x);
-  this.newHead.y = (this.newHead.y += y);
+  this.nextHead.x = (this.nextHead.x += x);
+  this.nextHead.y = (this.nextHead.y += y);
 }
 
 // Check that the snake did not collided with itself or with the edge of the board
 Snake.prototype.checkCollision = function() {
-  if ( this.newHead.x > canvas.width || this.newHead.y > canvas.height || snake.queue.indexOf('x:' + this.newHead.x + ', y:' + this.newHead.y) >= 0) {
-    game.stop();
+  if ( this.nextHead.x >= canvas.width || this.nextHead.x < 0 || this.nextHead.y > canvas.height || this.nextHead.y < 0 || this.queue.indexOf('x:' + this.nextHead.x + ', y:' + this.nextHead.y) >= 0) {
+    this.collision = true;
   };
-  //for( var i = 0; i < snake.queue.length; i++ ) {
-  //  if (snake.queue[i].x === snake.x && snake.queue[i].y === snake.y) {
-  //    game.stop();
-  //  }
-  //}
 }
 
+Snake.prototype.eat
 // Check whether the snake got food
 Snake.prototype.grow = function() {
   if ( this.head.x === food.coord.x && this.head.y === food.coord.y ) {
